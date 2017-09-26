ceead8c876f51e8e793c27a0d548e97bec07e2eb
Brett Peters <brett.peters1@gmail.com>
2017-09-26 16:19:43 -0600

    Add photo to card

--- app/src/components/RecipeCard.js
+++ app/src/components/RecipeCard.js
@@ -2,12 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
  
-const RecipeCard = ({ id, ingredients, name, special }) => (
+const RecipeCard = ({ id, ingredients, name, special, photoURL }) => (
   <div className="col-6 col-lg-4">
     <h2>{name}</h2>
+    {photoURL && 
+      <Link to={`/recipes/${id}`}>
+        <img src={photoURL} alt={name} className="img-fluid rounded mb-2" />
+      </Link>
+    }
     <ul>
-      {[...ingredients.map(ing => ing.name), ...special].map(name => (
-        <li key={name}>{name}</li>
+      {ingredients.map(ing => (
+        <li key={ing.name} className="text-muted">{ing.name}</li>
       ))}
     </ul>
     <p>
@@ -20,6 +25,7 @@ RecipeCard.propTypes = {
   id: PropTypes.string.isRequired,
   ingredients: PropTypes.array.isRequired,
   name: PropTypes.string.isRequired,
+  photoURL: PropTypes.string,
   special: PropTypes.array
 };
 
